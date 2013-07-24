ec944a83b187729b20d876dc2937947e6a8183c4
Kamal Ahmed <kamalmkg@hotmail.com>
2013-07-24 14:16:02 +0100

    removed old folder

--- old/include/footer.php
+++
@@ -1,5 +0,0 @@
-<div id="footer">
-	<a onClick="try_to_open_app()" href="fb://profile/212971392077465"><img src="images/facebook.png" alt="Facebook"/></a>
-	<a href="twitter://user?screen_name=joyrajonline"><img src="images/twitter.png" alt="Twitter"/></a>
-	<a class="shareLink" href="#"><img src="images/share.png" alt="Google Plus"/></a>
-</div>
